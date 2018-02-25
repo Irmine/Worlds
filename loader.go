c77eb7d0d0bdd4de283f3f8f8b9eb5633672153f
Sandertv <st.ten.veldhuis@gmail.com>
2018-02-26 00:13:07 +0100

    worlds/loader: Added unloading of unused chunks.

--- loader.go
+++ loader.go
@@ -1,19 +1,26 @@
 package worlds
 
-import "github.com/irmine/worlds/chunks"
+import (
+	"github.com/irmine/worlds/chunks"
+	"sync"
+)
 
 // Loader is a struct used to load a range of chunks from a dimension.
 type Loader struct {
-	Dimension    *Dimension
-	ChunkX       int32
-	ChunkZ       int32
+	Dimension *Dimension
+	ChunkX    int32
+	ChunkZ    int32
+
+	UnloadFunction func(chunk *chunks.Chunk)
+	LoadFunction   func(*chunks.Chunk)
+
+	mutex        sync.RWMutex
 	loadedChunks map[int]*chunks.Chunk
-	LoadFunction func(*chunks.Chunk)
 }
 
 // NewLoader returns a new loader on the given dimension with the given chunk X and Z.
 func NewLoader(dimension *Dimension, x, z int32) *Loader {
-	return &Loader{dimension, x, z, make(map[int]*chunks.Chunk), func(chunk *chunks.Chunk) {}}
+	return &Loader{dimension, x, z, func(chunk *chunks.Chunk) {}, func(chunk *chunks.Chunk) {}, sync.RWMutex{}, make(map[int]*chunks.Chunk)}
 }
 
 // Move moves the loader to the given chunk X and Z.
@@ -30,17 +37,22 @@ func (loader *Loader) Warp(dimension *Dimension, chunkX, chunkZ int32) {
 
 // HasChunkInUse checks if the loader has a chunk with the given chunk X and Z in use.
 func (loader *Loader) HasChunkInUse(chunkX, chunkZ int32) bool {
+	loader.mutex.RLock()
 	var _, ok = loader.loadedChunks[GetChunkIndex(chunkX, chunkZ)]
+	loader.mutex.RUnlock()
 	return ok
 }
 
 // setChunkInUse sets the given chunk in use.
 func (loader *Loader) setChunkInUse(chunkX, chunkZ int32, chunk *chunks.Chunk) {
+	loader.mutex.Lock()
 	loader.loadedChunks[GetChunkIndex(chunkX, chunkZ)] = chunk
+	loader.mutex.Unlock()
 }
 
 // Request requests all chunks within the given view distance from the current position.
 // All chunks loaded will run the load function of this loader.
+// Request will also unload any unused chunks beyond the distance specified.
 func (loader *Loader) Request(distance int32) {
 	var f = func(chunk *chunks.Chunk) {
 		loader.setChunkInUse(chunk.X, chunk.Z, chunk)
@@ -65,4 +77,21 @@ func (loader *Loader) Request(distance int32) {
 			}
 		}
 	}
+	loader.unloadUnused(distance)
+}
+
+// unloadUnused unloads all unused chunks beyond the given distance.
+func (loader *Loader) unloadUnused(distance int32) {
+	var rs = distance * distance
+	loader.mutex.Lock()
+	for index, chunk := range loader.loadedChunks {
+		xDist := loader.ChunkX - chunk.X
+		zDist := loader.ChunkZ - chunk.Z
+
+		if xDist*xDist+zDist*zDist > rs {
+			delete(loader.loadedChunks, index)
+			loader.UnloadFunction(chunk)
+		}
+	}
+	loader.mutex.Unlock()
 }
